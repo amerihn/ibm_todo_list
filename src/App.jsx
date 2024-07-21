9001bc3df173acdc4cc0aac849c6d7d4a71bbc67
amerihn <amerihn.art@gmail.com>
2024-07-21 17:04:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from './Components/TodoList';
+import Ref from './Components/Ref';
 
 function App() {
   return (
    <>
    <TodoList/>
+   <Ref/>
    </>
   );
 }
---
+++ src/Components/Ref.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Ref() {
+  const [text, setText] = React.useState("")
+  const [list, setList] = React.useState([])
+
+  function handleChange(e) {
+    setText(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!text) {
+      return;
+    }
+    setList(prevList => [...prevList, text])
+    setText("")
+  }
+  
+  return (
+    <>
+      <h2>React Project Ideas</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={text}
+          placeholder="Idea"
+        />
+        <button>Submit</button>
+      </form>
+
+      <ol>
+        {list.map((item, i) => <li key={i}>{item}</li>)}
+      </ol>
+    </>
+  )
+}
+export default Ref;
